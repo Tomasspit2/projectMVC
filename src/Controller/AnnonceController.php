64eaf5ccd3554eb97bac7e15b8b7ba1e8e855248
gibonDavid <gibon.david@live.fr>
2023-10-10 11:19:53 +0200

    verification in method add

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -69,32 +69,112 @@ class AnnonceController extends AbstractController
             ]
         );
     }
+
     /**
      * Add a new annonce
      * @return string|null
      */
     public function add(): ?string
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // clean $_POST data
-            $annonce = array_map('trim', $_POST);
-
-            // TODO validations (length, format...)
-
-            // if validation is ok, insert and redirection
-            $annonceManager = new AnnonceManager();
-            $id = $annonceManager->insert($annonce);
-
-            header('Location:/annonce/show?id=' . $id);
-            return null;
+        $annonce = $errors = [
+            'description' => '',
+            'photo' => '',
+            'annee' => '',
+            'puissance' => '',
+            'marque' => '',
+            'modele' => '',
+            'date_fin_enchere' => '',
+            'prix_depart' => '',
+            'titre_annonce' => ''
+        ];
+        function checkdata($data): string
+        {
+            $data = trim($data);
+            $data = htmlspecialchars($data);
+            $data = htmlentities($data);
+            return $data;
         }
 
-        return $this->twig->render('Annonce/add.html.twig');
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $uploadDir = "../public/assets/images/";
+            $extension = pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION);
+            $uploadFile = $uploadDir . $_FILES['photo']['name'];
+            move_uploaded_file($_FILES['photo']['tmp_name'], $uploadFile);
+            $authorizedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+            $maxFileSize = 1000000;
+
+            if ((!in_array($extension, $authorizedExtensions))) {
+                $errors['photo'] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou PNG ou gif ou webp.';
+            }
+            if (file_exists($_FILES['photo']['name']) && filesize($_FILES['photo']['tmp_name']) > $maxFileSize) {
+                $errors['photo'] = 'Voter fichier doit faire moins de 1Mo.';
+            }
+            if (!isset($_POST['description']) | empty(trim($_POST['description']))) {
+                $errors['description'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['description'] = checkdata($_POST['description']);
+            }
+            if (!isset($_POST['annee']) | empty(trim($_POST['annee']))) {
+                $errors['annee'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['annee'] = checkdata($_POST['annee']);
+            }
+            if (!isset($_POST['puissance']) | empty(trim($_POST['puissance']))) {
+                $errors['puissance'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['puissance'] = checkdata($_POST['puissance']);
+            }
+            if (!isset($_POST['marque']) | empty(trim($_POST['marque']))) {
+                $errors['marque'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['marque'] = checkdata($_POST['marque']);
+            }
+            if (!isset($_POST['modele']) | empty(trim($_POST['modele']))) {
+                $errors['modele'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['modele'] = checkdata($_POST['modele']);
+            }
+            if (!isset($_POST['date_fin_enchere']) | empty(trim($_POST['date_fin_enchere']))) {
+                $errors['date_fin_enchere'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['date_fin_enchere'] = checkdata($_POST['date_fin_enchere']);
+            }
+            if (!isset($_POST['prix_depart']) | empty(trim($_POST['prix_depart']))) {
+                $errors['prix_depart'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['prix_depart'] = checkdata($_POST['prix_depart']);
+            }
+            if (!isset($_POST['titre_annonce']) | empty(trim($_POST['titre_annonce']))) {
+                $errors['titre_annonce'] = 'Ce champs est obligatoire.';
+            } else {
+                $annonce['titre_annonce'] = checkdata($_POST['titre_annonce']);
+            }
+            if (
+                $errors['photo'] != ""
+                | $errors['description'] != ""
+                | $errors['annee'] != ""
+                | $errors['puissance']  != ""
+                | $errors['marque'] != ""
+                | $errors['modele'] != ""
+                | $errors['date_fin_enchere'] != ""
+                | $errors['prix_depart'] != ""
+                | $errors['titre_annonce'] != ""
+            ) {
+                return $this->twig->render('annonce/add.html.twig', ['error' => $errors, 'annonce' => $annonce]);
+            } else {
+                $productManager = new AnnonceManager();
+                $productManager->insert($_POST, $_FILES);
+                header('Location:/annonce/index');
+            }
+        }
+
+        return $this->twig->render('Annonce/add.html.twig', ['error' => $errors ,'annonce' => $annonce]);
     }
 
     /**
      * Delete a specific annonce
      * @param int $id
+     *  @throws \Exception
      */
     public function delete(int $id): void
     {
