01e12669719e0bb3d801abc5fe9df310bb510d87
Tomasspit2 <Tomas.p.spit1@gmail.com>
2023-10-11 09:15:14 +0200

    pull

--- src/Model/AuctionManager.php
+++ src/Model/AuctionManager.php
@@ -2,9 +2,22 @@
 
 namespace App\Model;
 
-use App\Model\AbstractManager;
-
 class AuctionManager extends AbstractManager
 {
+    public function selectAnnonceAndAuctionById(int $id): array
+    {
+        // Prepare the SQL query to retrieve data from both tables
+        $query = "
+        SELECT annonces.*, encheres.*
+        FROM annonces
+        LEFT JOIN encheres ON annonces.id_enchere = encheres.id
+        WHERE annonces.id=:id
+    ";
 
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
 }
